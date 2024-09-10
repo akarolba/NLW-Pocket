f438a1a02679f7a932468a7ceaadd776d590b666
Karolyne Barbosa <karolynebarbosaa@gmail.com>
2024-09-10 17:48:39 -0300

    objeto

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 // Hello World
 let mensagem = "Olá mundo!"
 console.log(mensagem)
+
+// arrays, objetos
+
+let metas = ["Karol, hello"]
+
+console.log(metas[1] + "," + metas [0])
+
+// objeto
+
+let meta = {
+  value: 'ler um livro por mês',
+  checked: false
+}
+
