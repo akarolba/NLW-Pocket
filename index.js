53386f8429af8ab65b9c9cc8585041b77e122bb3
Karolyne Barbosa <karolynebarbosaa@gmail.com>
2024-09-12 16:27:15 -0300

    metas abertas

--- index.js
+++ index.js
@@ -58,8 +58,24 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: "Metas Realizadas",
+    message: "Metas Realizadas" + realizadas.length,
     choices: [...realizadas]
+  })
+}
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+      return meta.checked != true
+  })
+
+  if(abertas.length == 0) {
+    console.log("Não existem metas abertas! :)")
+    return
+  }
+
+  await select({
+    message: "Metas Abertas" + abertas.length,
+    choices: [...abertas]
   })
 }
 
@@ -83,6 +99,10 @@ const start = async () => {
             value: "realizadas"
           },
           {
+            name: "Metas abertas",
+            value: "abertas"
+          },
+          {
             name: "Sair",
             value: "sair"
           }
@@ -101,6 +121,9 @@ const start = async () => {
           break
         case "realizadas":
           await metasRealizadas()
+          break
+        case "abertas":
+          await metasAbertas()
           break
         case "sair":
           console.log("Até a próxima!")
