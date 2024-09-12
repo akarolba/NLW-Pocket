5f218610696e677a4f46aa12b5ce6cb9d73d37d0
Karolyne Barbosa <karolynebarbosaa@gmail.com>
2024-09-12 13:02:25 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const {select} = require('@inquirer/prompts')
+const {select, input} = require('@inquirer/prompts')
+
+let meta = {
+    value: 'Tomar 2L de água por dia',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+  const meta = await input({message: "Digite a meta:"})
+
+  if(meta.length == 0) {
+      console.log('A meta não pode ser vazia.')
+      return
+  }
+
+  metas.push(
+    {value: meta, checked: false}
+  )
+}
 
 const start = async () => {
 
@@ -26,7 +46,8 @@ const start = async () => {
 
       switch (opção) {
         case "cadastrar":
-          console.log("vamos cadastrar")
+          await cadastrarMeta()
+          console.log(metas)
           break
         case "listar":  
           console.log("vamos listar")
