0cb07121f2ca3040704b8fe4a6456c6ce594fdca
Karolyne Barbosa <karolynebarbosaa@gmail.com>
2024-09-12 16:52:04 -0300

    Deletar metas

--- index.js
+++ index.js
@@ -74,10 +74,34 @@ const metasAbertas = async () => {
   }
 
   await select({
-    message: "Metas Abertas" + abertas.length,
+    message: "Metas Abertas: " + abertas.length,
     choices: [...abertas]
   })
 }
+
+const deletarMetas = async () => {
+  const metasDesmarcadas = metas.map((meta) => {
+      return {value: meta.value, checked: false}
+  })
+  const itemsADeletar = await checkbox({
+    message: "Selecione item para deletar",
+    choices: [...metasDesmarcadas],
+    instructions: false,
+  })
+
+  if(itemsADeletar.length == 0) {
+    console.log("Nenhum item para deletar!")
+    return
+  }
+
+  itemsADeletar.forEach((item) => {
+    metas = metas.filter(() => {
+      return meta.value != item
+    })
+  })
+
+  console.log("Meta(s) deletada(s) com sucesso! ;)")
+  }
 
 const start = async () => {
 
@@ -103,6 +127,10 @@ const start = async () => {
             value: "abertas"
           },
           {
+            name: "Deletar metas",
+            value: "deletar"
+          },
+          {
             name: "Sair",
             value: "sair"
           }
@@ -124,6 +152,9 @@ const start = async () => {
           break
         case "abertas":
           await metasAbertas()
+          break
+        case "deletar":
+          await deletarMetas()
           break
         case "sair":
           console.log("Até a próxima!")
