b34a7439bfe30b6c0657ddefed4765fc76cb57f6
Karolyne Barbosa <karolynebarbosaa@gmail.com>
2024-09-12 15:55:10 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -47,6 +47,22 @@ const listarMetas = async () => {
     console.log('Meta(s) marcadas como concluída(s)')
 }
 
+const metasRealizadas = async () => {
+  const realizadas = metas.filter((meta) => {
+      return meta.checked 
+  })
+
+  if(realizadas.length == 0) {
+    console.log('Não existem metas realizadas! :(')
+    return
+  }
+
+  await select({
+    message: "Metas Realizadas",
+    choices: [...realizadas]
+  })
+}
+
 const start = async () => {
 
     while(true) {
@@ -61,6 +77,10 @@ const start = async () => {
           {
             name: "Listar metas",
             value: "listar"
+          },
+          {
+            name: "Metas realizadas",
+            value: "realizadas"
           },
           {
             name: "Sair",
@@ -78,6 +98,9 @@ const start = async () => {
           break
         case "listar":  
           await listarMetas()
+          break
+        case "realizadas":
+          await metasRealizadas()
           break
         case "sair":
           console.log("Até a próxima!")
